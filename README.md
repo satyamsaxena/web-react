4ebbf76d55fee40bc2b0e14d56af9c09c9da51f4
satyamsaxena <satyamsaxena@outlook.com>
2021-02-16 16:38:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9gdt63
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-9gdt63)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div class="container">
+      <h1>Hello React!</h1>
+      <p>yes I am the one </p>
+      <p>and also The King Of lost Universe</p>
+    </div>
+  );
+}
