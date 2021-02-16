7f6878c87e6dc7f5edbd39fc762ed7310d92f535
satyamsaxena <satyamsaxena@outlook.com>
2021-02-16 16:47:55 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,9 +4,25 @@ import "./style.css";
 export default function App() {
   return (
     <div class="container">
-      <h1>Hello React!</h1>
-      <p>yes I am the one </p>
-      <p>and also The King Of lost Universe</p>
+      <div class="gfg-div">
+        <div class="gfg-logo" />
+        <p>HELLO</p>
+        <div class="gfg-title">Satyam</div>
+        <div class="gfg-sub-title">Login Form</div>
+        <div class="gfg-input-fields">
+          <div class="gfg-email fa fa-email">
+            <input type="email" placeholder="email" />
+          </div>
+          <div class="gfg-password fa fa-password">
+            <input type="password" placeholder="password" />
+          </div>
+        </div>
+        <button class="gfg-button">Ghazing Technosoft Signin</button>
+        <div class="gfg-link">
+          <a href="#">Forgot password?</a> or
+          <a href="#">Signup</a>
+        </div>
+      </div>
     </div>
   );
 }
